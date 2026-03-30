556105ae2327224a9aa012d534e812f427476fe5
nicola3102 <nicola.ni@networkprovide.com>
2026-03-30 16:26:38 +0800

    1:group a 部分当前week_no 时间问题

--- backend/app/services/online_sync.py
+++ backend/app/services/online_sync.py
@@ -1160,8 +1160,8 @@ def sync_from_online_db() -> dict:
 
     init_db()
 
-    # date_start_str, date_end_str = _get_sync_date_range()
-    date_start_str, date_end_str = "2026-02-15", "2026-02-22"
+    date_start_str, date_end_str = _get_sync_date_range()
+    # date_start_str, date_end_str = "2026-03-22", "2026-03-29"
     logger.info("Sync date range: date_start=%s, date_end=%s", date_start_str, date_end_str)
 
     # week_no 以 date_end 减 1 天为参考日，保证 02-21～02-22 写入 202607（02-15～02-21 所在周）
